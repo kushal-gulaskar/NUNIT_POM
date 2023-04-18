76792d2758c32663c84142d9c9f540b5082dc3c3
gulaskar <kush6188@gmail.com>
2023-04-18 18:05:01 +0530

    test

--- NUNIT/Tests/LoginOrangeHrm.cs
+++ NUNIT/Tests/LoginOrangeHrm.cs
@@ -1,15 +1,10 @@
 ﻿using NUnit.Framework;
 using NUNIT.Pages;
 using NUNIT.Utilities.hooks;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NUNIT.Tests
 {
-   
+
     class LoginOrangeHrm:TestListner
     {
         LoginOrangeHRMPage loginpage = new LoginOrangeHRMPage();
